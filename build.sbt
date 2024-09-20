30c6726de7a209c05e4105a49ab67db8082d5454
Marco Zühlke <mzuehlke@gmail.com>
2024-09-20 14:18:15 +0200

    create minimal project to reproduce issue

---
+++ build.sbt
@@ -0,0 +1,3 @@
+ThisBuild / scalaVersion := "3.3.1"
+
+libraryDependencies += Dependencies.cats
---
+++ project/Dependencies.scala
@@ -0,0 +1,5 @@
+import sbt.*
+
+object Dependencies {
+  val cats = "org.typelevel" %% "cats-core" % "2.10.0"
+}
